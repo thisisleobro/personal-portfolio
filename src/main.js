309c640f490cb57d5d95785d43f1ca8cdcc3c664
Leo Carvalho <25367871+thisisleobro@users.noreply.github.com>
2024-12-06 08:56:58 -0100

    Compile time dom manipulation with node-html-parser to load github repos

--- src/main.js
+++ src/main.js
@@ -1,4 +1,52 @@
 import './style.css'
-import { setupCounter } from './counter.js'
+// import { setupCounter } from './counter.js'
+// import repos from '../repos.json'
 
-setupCounter(document.querySelector('#counter'))
+
+// const repoElementsContainer = document.querySelector('#repos')
+
+// repos.forEach((repo) => {
+// 	const title = document.createElement("p")
+// 	title.textContent = repo.description ?? 'No description'
+	
+// 	const stars = document.createElement("p")
+// 	stars.textContent = repo.stargazers_count
+	
+// 	const url = document.createElement("a")
+// 	url.textContent = 'url'
+// 	url.href = repo.html_url
+// 	url.className = 'text-violet-500'
+
+// 	const repoElement = document.createElement('div')
+// 	repoElement.appendChild(title)
+// 	repoElement.appendChild(stars)
+// 	repoElement.appendChild(url)
+
+// 	repoElementsContainer.appendChild(repoElement)
+// })
+
+// setupCounter(document.querySelector('#counter'))
+
+
+const sections = document.querySelectorAll("body > section");
+const navLi = document.querySelectorAll("#top-navigation > a");
+
+window.addEventListener('scroll', () => {
+	console.log('scroll')
+	var currentSectionId = "";
+
+	sections.forEach((section) => {
+		const sectionTop = section.offsetTop;
+		if (pageYOffset >= sectionTop - 300) {
+			currentSectionId = section.getAttribute("id");
+		}
+	});
+
+	navLi.forEach((li) => {
+		li.classList.remove("active");
+
+		if (li.href.includes(currentSectionId)) {
+			li.classList.add('active');
+		}
+	});
+}, { passive: true })

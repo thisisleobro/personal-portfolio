c87daf93a934d51960d221e59d722c38d78066d9
Leo Carvalho <25367871+thisisleobro@users.noreply.github.com>
2025-08-24 00:34:33 -0100

    fix: sitemap filter

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig({
   integrations: [
     tailwind(),
     sitemap({
-      filter: (page) => page.startsWith(`${site}/blog`),
+      filter: (page) => !page.startsWith(`${site}/blog`),
       i18n: {
         defaultLocale, // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
         locales,

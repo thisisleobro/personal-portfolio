de47ce6aeab6174919117201681b282125bff937
Leo Carvalho <25367871+thisisleobro@users.noreply.github.com>
2025-12-07 21:17:24 -0100

    fix: broken locale causing sitemap to not be generated

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from "@astrojs/sitemap";
 
 import { defaultLocale } from './src/constants';
-import {localeKeys, locales} from '/src/lib/i18n'
+import { localeKeys } from './src/lib/i18n'
 
 const site = process.env.SITE_URL || 'http://localhost:4321'
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -27,7 +28,7 @@ export default defineConfig({
       // filter: (page) => !page.startsWith(`${site}/blog/`) && !page.startsWith(`${site}/pt/blog/`),
       i18n: {
         defaultLocale, // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
-        locales,
+        locales: localeKeys.reduce((locales, localeKey) => (locales[localeKey] = localeKey, locales), {}),
       },
     }),
   ]

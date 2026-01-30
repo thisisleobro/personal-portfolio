568a8023b08a0b6fe65549c8ae2b83ed8b177edf
Leo Carvalho <25367871+thisisleobro@users.noreply.github.com>
2026-01-30 12:34:46 -0100

    feat: some SEO improvements. Added Open Graph / Facebook and Twitter tags

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ export const siteName: string = 'Leo Carvalho'
 export const defaultLocale: string = 'en'
 export const email: string = 'leo@carvalho.cv'
 
-// export const author: string = 'Leo Carvalho'
+export const author: string = 'Leo Carvalho'
--- src/content.config.ts
+++ src/content.config.ts
@@ -8,14 +8,14 @@ const blogCollection = defineCollection({
 		title: z.string(),
 		description: z.string().optional(),
 		author: z.string().optional(),
-		reference: z.string().optional(),
+		// reference: z.string().optional(),
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		previewImage: z.string().optional(),
-		secret: z.boolean().default(false),
+		// secret: z.boolean().default(false),
 		tags: z.array(z.string()).default(['other']),
-		canonicalURL: z.string().optional(),
+		// canonicalURL: z.string().optional(),
 	}),
 });
 
